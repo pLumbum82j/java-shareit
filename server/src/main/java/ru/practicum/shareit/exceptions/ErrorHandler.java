7b8450a713f922cb20eebbdac1ff1df131b33756
pLumbum82 <jobbsy@yandex.ru>
2023-05-20 20:13:34 +0500

    style: снова лишний импорт забыл убрать

--- server/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -3,13 +3,9 @@ package ru.practicum.shareit.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
-import java.util.Date;
 import java.util.Map;
 
 /**
